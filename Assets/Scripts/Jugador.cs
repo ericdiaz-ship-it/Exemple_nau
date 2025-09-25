d23bc389bc61e30599de3516549b0b4bb50c710d
Eric Díaz Bueno <ericdiaz@ceroca.cat>
2025-09-25 17:56:42 +0200

    Control limits

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Jugador : MonoBehaviour
@@ -5,18 +6,25 @@ public class Jugador : MonoBehaviour
     public GameObject jugador;
     public float vel = 10f;//en c# se suele poner el tipo de valor al final
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Camera camera;
     void Start()
     {
-
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-        {
 
-        }
+        MovimentNau();
+
+
+    }
+    void MovimentNau()
+    {
+        Vector3 limitInferiorEsquerra = camera.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector3 limitSuperiorDret = camera.ViewportToWorldPoint(new Vector2(1, 1));
+
         float direccioHorizontal = Input.GetAxisRaw("Horizontal");
         float direccioVertical = Input.GetAxisRaw("Vertical");
         Vector3 direccio = new Vector3(direccioHorizontal, direccioVertical, 0).normalized;
@@ -26,6 +34,9 @@ public class Jugador : MonoBehaviour
             vel * direccio.y * Time.deltaTime,
             vel * direccio.z * Time.deltaTime);
         transform.position += nouDesplacament;
-
+        Vector3 novapos = transform.position;
+        nouDesplacament.x = Math.Clamp(novapos.x, limitInferiorEsquerra.x, limitSuperiorDret.x);
+        nouDesplacament.y = Math.Clamp(novapos.y, limitInferiorEsquerra.y, limitSuperiorDret.y);
+        
     }
 }
